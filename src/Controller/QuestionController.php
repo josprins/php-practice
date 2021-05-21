5b983657b897e37d4e5065ff8df8548a5a4c4ad3
josprins <jco.prins@gmail.com>
2021-05-21 11:34:32 +0200

    final commit

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -7,19 +7,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
     /**
-     *@Route("/")
+     *@Route("/", name="app_homepage")
      */
-    public function homepage()
+    public function homepage(Environment $twigEnvironment)
     {
-        return new Response('What a beautiful day!');
+//        $html = $twigEnvironment->render('question/homepage.html.twig');
+//        return new Response($html);
+
+        return $this->render('question/homepage.html.twig');
     }
 
     /**
-     *@Route("/show/{slug}")
+     *@Route("/show/{slug}", name="app_question_show")
      */
     public function show($slug)
     {
@@ -29,6 +33,8 @@ class QuestionController extends AbstractController
             "The moon is grey ",
             "I love icecream "
         ];
+
+        dump($slug, $this);
 
         return  $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
